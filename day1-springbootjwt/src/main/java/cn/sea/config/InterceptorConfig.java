fdffd46ec960370c783a116017d2d0909279ff93
codersea <49507860+luxianghai@users.noreply.github.com>
2020-11-29 13:25:32 +0800

    Update InterceptorConfig.java

--- day1-springbootjwt/src/main/java/cn/sea/config/InterceptorConfig.java
+++ day1-springbootjwt/src/main/java/cn/sea/config/InterceptorConfig.java
@@ -3,6 +3,7 @@ package cn.sea.config;
 import cn.sea.interceptors.JWTInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,11 @@ public class InterceptorConfig implements WebMvcConfigurer {
         registry.addInterceptor(new JWTInterceptor())
                 .addPathPatterns("/user/test") // 其它接口都保护，token验证
                 .excludePathPatterns("/user/login"); // 所有用户都放行
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/swagger-ui.html");
     }
 }
